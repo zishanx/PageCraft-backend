86656e18a1fd9b8b06aade23a74dd1338b95c240
zishanx <zishanalikhan53@gmail.com>
2026-05-07 21:41:48 +0530

    Deploying

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ app.use(express.json())
 app.use(cors())
 dotenv.config()
 
-mongoose.connect(process.env.MONGO_URI).then(()=>{console.log("MongoDb connected")}).catch(err=>console.log(err))
+mongoose.connect(process.env.MONGO_URI).then(() => { console.log("MongoDb connected") }).catch(err => console.log(err))
 
 const projectRoutes = require('./routes/project')
 app.use('/api/projects', projectRoutes)
 
 const PORT = process.env.PORT
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
+})
+
+app.get("/", (req, res) => {
+    res.send("Backend Running")
 })
